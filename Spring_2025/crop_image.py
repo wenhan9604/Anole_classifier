ac63c46c7a9a3d14e089168e3507a3a6fc97271d
Wen Han <wenhan9604@gmail.com>
2025-03-12 23:05:47 +0800

    feat: implemented crop and resize for entire image folder

--- Spring_2025/crop_image.py
+++ Spring_2025/crop_image.py
@@ -1,5 +1,4 @@
 import cv2 
-from pathlib import Path
 import numpy as np
 
 def crop_image(img_path, coord):
@@ -7,12 +6,11 @@ def crop_image(img_path, coord):
 
     input = cv2.imread(img_path)
 
-    print("input shape: ", input.shape)
+    # print("input shape: ", input.shape)
 
-
-    cv2.imshow('image', input) 
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows() 
+    # cv2.imshow('image', input) 
+    # cv2.waitKey(0) 
+    # cv2.destroyAllWindows() 
 
     x_center, y_center, width, height = coord
 
@@ -24,11 +22,11 @@ def crop_image(img_path, coord):
 
     cropped_segment = input[row_start:row_end, col_start:col_end]
 
-    print("Cropped shape: ", cropped_segment.shape)
+    # print("Cropped shape: ", cropped_segment.shape)
 
-    cv2.imshow('cropped', cropped_segment) 
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows() 
+    # cv2.imshow('cropped', cropped_segment) 
+    # cv2.waitKey(0) 
+    # cv2.destroyAllWindows() 
 
     return cropped_segment
 
@@ -50,35 +48,30 @@ def get_coord(labels_file_path):
     height = np.floor(height * 640)
 
     # Print values to verify
-    print(x_center, y_center, width, height)
+    # print(x_center, y_center, width, height)
 
     return (x_center, y_center, width, height)
 
-def resize_img(img, size):
-    """
-    Args:
-        img(numpy array)
-        size(int,int) = (width, height)
-    """
-
-    resized_img = cv2.resize(img, size)
-
-    return resized_img
-
-def crop_resize_img_folder(src_folder_dir, dest_folder_dir):
+def crop_resize_img_folder(src_folder_dir, dest_folder_dir, resize_value):
     """
     Will crop and resize images from source folder and store in destination folder
+
+    Args:
+        resize_value(int, int) : The size of the final resized images (width, height)
     """
 
     if not src_folder_dir.is_dir():
         print(f"Source folder '{src_folder_dir}' does not exist.")
         return
 
+    src_img_folder = src_folder_dir + "/images"
+    src_txt_folder = src_folder_dir + "/labels"
+
     # Create destination folder if it doesn't exist
     dest_folder_dir.mkdir(parents=True, exist_ok=True)
 
     # Get all files sorted by modification time (most recent first)
-    images = sorted(src_folder_dir.glob('*'), key=lambda img_file: img_file.name)
+    images = sorted(src_img_folder.glob('*'), key=lambda img_file: img_file.name)
     
     # Filter to ensure we only copy files, not directories
     images = [img_file for img_file in images if img_file.is_file()]
@@ -86,23 +79,31 @@ def crop_resize_img_folder(src_folder_dir, dest_folder_dir):
     count = 0
     for image in images:
 
-        img_name = "36391_5983992_jpg.rf.820094a9f215cce647b3d64d5bc0f5dc"
+        img_name = image.stem
 
-        img_path = "C:/Projects/OMSCS/Lizard_Classification/Anole_classifier/Dataset/YOLO_training/lizard_10000/train/images/" + img_name + ".jpg"
-        labels_file_path = "C:/Projects/OMSCS/Lizard_Classification/Anole_classifier/Dataset/YOLO_training/lizard_10000/train/labels/" + img_name + ".txt"
+        img_path = image
+        labels_file_path = src_txt_folder + img_name + ".txt"
+
+        #Core functions: Crop and Resize images
+        coord = get_coord(labels_file_path)
+        cropped_image = crop_image(img_path, coord)
+        resized_img = cv2.resize(cropped_image, resize_value)
 
         dest_img_name = img_name + "_cropped"
         dest_path = dest_folder_dir + dest_img_name + ".jpg"
 
-        coord = get_coord(labels_file_path)
-        cropped_image = crop_image(img_path, coord)
-        cv2.imwrite(dest_path, cropped_image)
+        cv2.imwrite(dest_path, resized_img)
 
         count += 1
         print(f"Image count: {count}")
 
-    print("Successfully Saved ")
+    print("Images Successfully cropped, resized and saved")
 
+src_folder = "../Dataset/YOLO_training/knightanole_2000"
+dest_folder = "../Dataset/YOLO_training/knightanole_2000_cropped"
+resize_value = (320, 320)
+
+crop_resize_img_folder(src_folder, dest_folder, resize_value)
 
 
 
