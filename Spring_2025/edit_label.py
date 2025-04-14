48708687348777cef49041dddab89b89ecd3aeb0
Wen Han <wenhan9604@gmail.com>
2025-04-15 07:54:37 +0800

    feat: implemented edit label python code

---
+++ Spring_2025/edit_label.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+def edit_label(root_dir, target_char):
+    """
+    Arg:
+        root_dir: Will edit all text files found under root directory 
+        target_char: Will replace the first char of each line with target_char
+    """
+
+    root_dir_path = Path(root_dir)
+
+    for txt_file in root_dir_path.glob("**/*.txt"):  # "**/*.txt" finds all .txt files recursively
+        print(f"Processing {txt_file}")
+        
+        with open(txt_file, 'r+') as file:
+            lines = file.readlines()
+            file.seek(0)
+            for line in lines:
+                if line.strip():
+                    file.write(target_char + line[1:])
+                else:
+                    file.write(line)
+            file.truncate()
+
+
+root_dir = "C:/Projects/OMSCS/Lizard_Classification/Anole_classifier/Dataset/YOLO_training/original_train_val_test_split_labelled/knightanole_2000/"
+
+target_char = "4"
+
+edit_label(root_dir=root_dir, target_char=target_char)
